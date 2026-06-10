4989e9caad0586dadb59e88fd2196b1667e8c8a6
MCREYNOLD <ngadeulrich@gmail.com>
2026-06-10 21:55:12 +0100

    Fix compatibilite PostgreSQL

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, render_template
 from ..models import Book, Category
+from sqlalchemy import true
 
-main_bp = Blueprint('main', __name__)
+main_bp = Blueprint('main', name)
 
 @main_bp.route('/')
 def index():
-    featured = Book.query.filter_by(is_featured=True).limit(6).all()
+    featured = Book.query.filter(Book.is_featured == true()).limit(6).all()
     nouveautes = Book.query.order_by(Book.created_at.desc()).limit(8).all()
     bestsellers = Book.query.order_by(Book.note_moyenne.desc()).limit(8).all()
     categories = Category.query.all()
